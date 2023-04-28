8558cc505f61cb5fa5871d3ac8ba448ccbca75a2
cesarjanuario82 <87102305+cesarjanuario82@users.noreply.github.com>
2023-04-28 09:51:36 -0600

    Update useServices.js
    se agrega el servicio dummy para el reporte

--- src/services/useServices.js
+++ src/services/useServices.js
@@ -8,6 +8,8 @@ export const useServices = () => {
     const [isLoading, setIsLoading] = useState(false);
 
     const urlApi = '/api/';
+    const urlApiReporte ='https://cesarjanuario82.github.io/tiendas/reportewm.json';  //este es un json  dummy simula el resultado del servicio de reportes
+
 
     const enviarMensaje = (payload) => {
 
@@ -37,8 +39,41 @@ export const useServices = () => {
         return result;
     };
 
-    return {
-        enviarMensaje,
-        isLoading
+
+const solicitarReporte = (dn) => {
+
+    const request =   {
+        url: urlApiReporte,
+        //method: 'POST',
+        method: 'GET',//aqui lo puse GET solo para consumir el json dummy
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        //data: dn,
     }
+   
+    console.log('REQ REPORTE: ', request);
+    const result =axios(request)
+    .then(response => {
+       // console.log('servicio reporte responde: ', response.data);
+        setIsLoading(false);
+            return response.data;
+        })
+        .catch((error) => {
+            setIsLoading(false);
+            console.error('servicio reporte error: ',error);
+            return error.response;
+        });
+
+    return result;
+}
+
+
+
+  
+return {
+        enviarMensaje,
+        solicitarReporte,
+        isLoading
+}
 }
