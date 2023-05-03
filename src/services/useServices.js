a18e39f19e383832c2e5e5952ef51406fc742d35
cesarjanuario82 <87102305+cesarjanuario82@users.noreply.github.com>
2023-05-02 22:14:19 -0600

    Add files via upload

--- src/services/useServices.js
+++ src/services/useServices.js
@@ -9,7 +9,9 @@ export const useServices = () => {
 
     const urlApi = '/api/';
 
-    const enviarMensaje = (payload) => {
+const urlReporte = 'https://db81-192-151-180-180.ngrok-free.app/readMessages/521'
+
+const enviarMensaje = (payload)  => {
 
         setIsLoading(true);
 
@@ -37,8 +39,44 @@ export const useServices = () => {
         return result;
     };
 
-    return {
+
+const solicitarReporte = (dn) => {
+
+    setIsLoading(true);
+
+    const request =   {
+       // url: `https://db81-192-151-180-180.ngrok-free.app/readMessages/521${dn}`,
+        url: urlReporte+dn,
+        method: 'GET',
+        headers: {
+        'ngrok-skip-browser-warning':'allow',
+        'Content-Type': 'application/json',
+        },
+        //data: dn,
+    }
+   
+    console.log('REQ REPORTE: ', request);
+    const result =axios(request)
+    .then(response => {
+       console.log('servicio reporte responde: ', response.data);
+        setIsLoading(false);
+            return response.data;
+        })
+        .catch((error) => {
+            setIsLoading(false);
+            console.error('servicio reporte error: ',error);
+            return error.response;
+        });
+
+    return result;
+}
+
+
+
+  
+return {
         enviarMensaje,
+        solicitarReporte,
         isLoading
     }
 }
